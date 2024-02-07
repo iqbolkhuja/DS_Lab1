563bdfa24304e4e049fc3a3d18af2cabacd0fcf3
iqbolkhuja <i.temirkhojaev@centre.edu>
2024-02-07 13:03:48 -0500

    iqbolio

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -9,7 +9,25 @@ public class Lab1 {
 	   public static int squares(int n)
 	   {
 	   
-	      return 0;  
+		   if (n<0)
+
+		   {
+
+			   return -1;
+
+		   }
+
+		   int answer = 0;
+
+		   for (int i=1;i<=n;i++)
+
+		   {
+
+			   	answer+=i*i;
+
+		   }
+
+		   return answer;
 	   
 	   }
 
@@ -23,7 +41,37 @@ public class Lab1 {
 	   public static int maxIndex(int array[],int len)
 	   {
    
-	      return 0;
+
+		   if (len<1)
+
+		   {
+
+			   return -1;
+
+		   }
+
+		   int maxValue = array[0];
+
+		   int maxIndexNumber = 0;
+
+		   for (int i=0;i<len;i++)
+
+		   {
+
+			   if (array[i] > maxValue)
+
+			   {
+
+				   maxValue = array[i];
+
+				   maxIndexNumber = i;
+
+			   }
+
+		   }
+
+		   return maxIndexNumber;
+
 	   }
 
 	/**
@@ -35,8 +83,16 @@ public class Lab1 {
 	   public static boolean seek(int array[],int len,int target)
 	   {
    
-	      return false;      
-	  	
+		   for (int i=0;i<len;i++){
+
+			   if (target == array[i]){
+
+			   return true;
+
+			   }
+
+		   }
+		   return false;
 	      
 	   }
 
@@ -47,11 +103,22 @@ public class Lab1 {
 	* sequence.  The first 6 numbers of the sequence are as follows.  1,1,2,3,5,8.  Note
 	* that 8 is the 6th number.  Be sure that your algorithm can return the
 	* first and second Fibonacci numbers correctly.  You should solve this without recursion. 
-	* If the input is below 1, retun 0;
+	* If the input is below 1, retun 0; IQBOLIO
 	*/
-	   public static int fib(int n)
+	   public static int fib (int n)
 	   {
-	      return 0;
+	      if (n<1) {return 0;}
+	      if (n==1 || n==2) {return 1;}
+	      int firstFibNumber = 1;
+	      int secondFibNumber = 1;
+	      int currentFibNumber = 0;
+	      for (int i=3;i<=n;i++)
+	      {
+	    	  currentFibNumber = firstFibNumber+secondFibNumber;
+	    	  firstFibNumber = secondFibNumber;
+	    	  secondFibNumber = currentFibNumber;
+	      }
+	      return currentFibNumber;
 	   }
 
 }
